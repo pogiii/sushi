c120b79dcdc639e736d329a9dd22f69545e02f51
Aviv Ben Yosef <avivbyo@gmail.com>
2024-05-19 11:59:24 +0300

    Finished OTP Input

--- registery/components/inputs/OTPInput.tsx
+++ registery/components/inputs/OTPInput.tsx
@@ -1,6 +1,5 @@
-import { ComponentProps, useEffect, useRef, useState } from "react";
-import { TextInput } from "react-native";
-import { Button, Form, Input, styled, XStack, YStack } from "tamagui";
+import { ComponentProps, useState } from "react";
+import { Form, Input, styled, XStack, YStack } from "tamagui";
 
 type OTPInputProps = ComponentProps<typeof Form> & { length: number };
 
@@ -11,16 +10,46 @@ export const OTPInput = (props: OTPInputProps) => {
         width: "$4"
     })
 
-    const ref = useRef<TextInput[]>(new Array(props.length));
+    const [focusArray, setFocusArray] = useState<boolean[]>(new Array(props.length).fill(false));
+    const [valueArray, setValueArray] = useState<number[]>(new Array(props.length));
 
-    const pushRef = (el: TextInput) => ref.current.push(el);
-    
+
+    // For some reason using this approach to move the focus forces me to keep a values array.
+    const focusOnNextInput = (e: number, i: number): void => {
+        const _focusArray = [...focusArray];
+        const _valueArray = [...valueArray];
+        const next = i + 1;
+
+        _valueArray[i] = e;
+
+        setValueArray(_valueArray);
+
+        if (String(e) === '') {
+            _focusArray[i] = true;
+            setFocusArray(_focusArray);
+            return;
+        }
+
+        console.log(e, "no")
+
+        if (_focusArray[next] === undefined) {
+            setFocusArray(new Array(props.length).fill(false));
+            return;
+        }
+
+        _focusArray[i] = false;
+        _focusArray[next] = true;
+
+        setFocusArray(_focusArray);
+
+    }
+
     return (
         <Form {...props}>
             <YStack>
                 <XStack gap={"$4"}>
                     {[...Array(props.length)].map((_, i) => (
-                        <OTPNumberInput ref={pushRef} textAlign="center" maxLength={1} key={i} />
+                        <OTPNumberInput value={valueArray[i]} autoFocus={focusArray[i]} onChangeText={(e) => { focusOnNextInput(e, i) }} textAlign="center" maxLength={1} key={i} />
                     ))}
                 </XStack>
             </YStack>
