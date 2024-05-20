c563b8c6b3103cfc86329672dacc3384ec9eff2a
Aviv Ben Yosef <avivbyo@gmail.com>
2024-05-20 18:00:25 +0300

    added tamagui config

--- components/TamaguiWrapper.tsx
+++ components/TamaguiWrapper.tsx
@@ -1,15 +1,16 @@
-import { TamaguiProvider, createTamagui } from 'tamagui'
+import { TamaguiProvider} from 'tamagui'
+import {tamaguiConfig} from "../tamagui.config"
 import { config } from '@tamagui/config/v3'
 import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
 
-// you usually export this from a tamagui.config.ts file
-const tamaguiConfig = createTamagui(config)
+// // you usually export this from a tamagui.config.ts file
+// const tamaguiConfig = createTamagui(config)
 
-// make TypeScript type everything based on your config
-type Conf = typeof tamaguiConfig
-declare module 'tamagui' { // or 'tamagui'
-    interface TamaguiCustomConfig extends Conf { }
-}
+// // make TypeScript type everything based on your config
+// type Conf = typeof tamaguiConfig
+// declare module 'tamagui' { // or 'tamagui'
+//     interface TamaguiCustomConfig extends Conf { }
+// }
 
 type TamaguiWrapperProps = {
     children?: React.ReactNode
